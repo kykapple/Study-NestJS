e36f39fa0d3007ce59e5e80471549a949fda36c6
kykapple <kykapple@naver.com>
2021-10-31 23:25:13 +0900

    jenkins webhook test

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 dotenv.config();
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 3000;
+
+  app.use(cookieParser());
+  app.setGlobalPrefix('/api');
+  app.enableCors({
+    credentials: true,
+  })
+
+  await app.listen(PORT);
 }
 bootstrap();
